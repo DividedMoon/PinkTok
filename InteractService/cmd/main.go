c2da222ec8d05683c61724300f0d84566ca5d6d9
HongjiangLiu <352041429@qq.com>
2023-09-04 21:11:27 +0800

    feat: add tcp port at 11013

--- InteractService/cmd/main.go
+++ InteractService/cmd/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/kitex/server"
 	"interact_service/biz/handler"
 	biz "interact_service/biz/interactservice"
+	"interact_service/internal/model"
 	"log"
+	"net"
 )
 
 func main() {
-	svr := biz.NewServer(new(handler.InteractServiceImpl))
+	addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:11013")
+	if err != nil {
+		hlog.Error(err)
+	}
+	model.InitDB()
+	svr := biz.NewServer(new(handler.InteractServiceImpl), server.WithServiceAddr(addr))
 
-	err := svr.Run()
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
